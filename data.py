acc73d076f6460ce609b1e3b0da6c0135e11552e
borischen425 <96858963+borischen425@users.noreply.github.com>
2024-09-06 06:25:05 +0800

    data.py

--- data.py
+++ data.py
@@ -202,5 +202,5 @@ results = scraper.scrape_all(keyword, num_pages=2)
 if keyword:
     df = pd.DataFrame(results, columns=("title", "price", "link", "source"))
     st.dataframe(df, use_container_width=True, column_config={
-        "website": st.column_config.LinkColumn()
+        "link": st.column_config.LinkColumn()
     })
